ceb5200fe7a9e882f3483c9c6d43545617491e69
student <student@DESKTOP-I3OGHQQ>
2025-03-06 16:19:18 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class FizzBuzz
+    {
+        private int _upperLimit;
+        public FizzBuzz(int upperLimit)
+        {
+            _upperLimit = upperLimit;
+        }
+        public void PrintNumbers()
+        {
+            for (int i = 1; i <= _upperLimit; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Podaj zakres liczb:");
+            if (int.TryParse(Console.ReadLine(), out int upperLimit))
+            {
+                FizzBuzz fizzBuzz = new FizzBuzz(upperLimit);
+                fizzBuzz.PrintNumbers();
+            }
+            else
+            {
+                Console.WriteLine("Złe dane");
+            }
+        }
+    }
+
+}
